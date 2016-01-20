a4d115cd06d64772933e367c9ed593fe5ece47aa
antony-wilson <antony.wilson@stfc.ac.uk>
2016-01-20 09:06:43 +0000

    seperate top concept for climate4impact

--- vocabularies/generate_climate4impact_ttl.py
+++ vocabularies/generate_climate4impact_ttl.py
@@ -3,9 +3,9 @@ from settings import C4I
 
 
 def generate():
-    concept.write_ttl('climate4impact-themes.csv', 'climate4impact-themes.ttl', 'Theme', 'Theme', C4I)
+    concept.write_ttl(C4I)
 
-    scheme.write_ttl('climate4impact-schemes.csv', 'climate4impact-schemes.ttl', C4I)
+    scheme.write_ttl(C4I)
 
 
 if __name__ == "__main__":
