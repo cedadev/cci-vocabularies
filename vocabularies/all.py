7a2d9c56a0e98849ed95873c331e8a1563db3b39
antony-wilson <antony.wilson@stfc.ac.uk>
2016-01-21 12:51:23 +0000

    add test to ensure office is not running

--- vocabularies/all.py
+++ vocabularies/all.py
@@ -1,11 +1,21 @@
 import time
 
-import generate_all_ttl, generate_csv, generate_html, generate_rdf, deploy_rdf
+import generate_all_ttl
+import generate_csv
+import generate_html
+import generate_rdf
+import deploy_rdf
 
 
 def generate():
+    start_time = time.time()
     print "%s generate_csv" % (time.strftime("%H:%M:%S"))
     generate_csv.generate()
+    end_time = time.time()
+    if end_time - start_time < 0.5:
+        print end_time - start_time
+        print "Is Office still open?"
+        exit()
     print "%s generate_all_ttl" % (time.strftime("%H:%M:%S"))
     generate_all_ttl.generate()
     print "%s generate_rdf" % (time.strftime("%H:%M:%S"))
