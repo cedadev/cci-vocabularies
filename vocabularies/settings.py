7221a3213baf761b02666bc673fd82fc6ff1e812
dwest77a <daniel.westwood@stfc.ac.uk>
2025-02-24 16:40:53 +0000

    Added hook for Docker image

--- vocabularies/settings.py
+++ vocabularies/settings.py
@@ -106,7 +106,7 @@ COLLECTION_MAP[GCOS] = GCOS_COLLECTION
 COLLECTION_MAP[GRIB] = GRIB_COLLECTION
 COLLECTION_MAP[GLOSSARY] = GLOSSARY_COLLECTION
 
-BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+BASE_PATH = os.environ.get('DEPLOY_PATH') or os.path.abspath(os.path.dirname(__file__))
 CSV_DIRECTORY = os.path.join(BASE_PATH, "..", "csv")
 DATA_DIRECTORY = os.path.join(BASE_PATH, "..", "data")
 HTML_DIRECTORY = os.path.join(BASE_PATH, "..", "html")
